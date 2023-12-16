47955d8919205546fa3a286be7feb5c34a3f6306
pya-h <p4ya.py@gmail.com>
2023-12-16 13:44:17 +0330

    Updates

---
+++ precache-manifest.3d7caf9199d12be6f8869fc370d4314d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10164dc61d81f66561ee788b070d079",
+    "url": "/Pol-Sazeh/index.html"
+  },
+  {
+    "revision": "eca6289e0b93f959c1a6",
+    "url": "/Pol-Sazeh/static/css/2.b821fc4b.chunk.css"
+  },
+  {
+    "revision": "067610df5a9c1c07c152",
+    "url": "/Pol-Sazeh/static/css/main.bc6811bf.chunk.css"
+  },
+  {
+    "revision": "eca6289e0b93f959c1a6",
+    "url": "/Pol-Sazeh/static/js/2.fe18d4a6.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/Pol-Sazeh/static/js/2.fe18d4a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067610df5a9c1c07c152",
+    "url": "/Pol-Sazeh/static/js/main.75745553.chunk.js"
+  },
+  {
+    "revision": "a2715f2028ea93a6607c",
+    "url": "/Pol-Sazeh/static/js/runtime-main.d44cb0be.js"
+  },
+  {
+    "revision": "00502a312e4bf5e53360a5949f579e29",
+    "url": "/Pol-Sazeh/static/media/img_mehrzad_0.00502a31.jpg"
+  },
+  {
+    "revision": "00b26b61df26201f7627a41827faaea4",
+    "url": "/Pol-Sazeh/static/media/img_mehrzad_1.00b26b61.jpg"
+  },
+  {
+    "revision": "1edca19a71d4789cacd20ef42d1a3993",
+    "url": "/Pol-Sazeh/static/media/img_mehrzad_2.1edca19a.jpg"
+  }
+]);
